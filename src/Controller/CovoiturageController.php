19f6dad5e7b1c88009554f794242b18c5e4b193f
samvht <samvht@hotmail.com>
2025-01-09 13:41:55 +0100

    Front-end covoiturage

--- src/Controller/CovoiturageController.php
+++ src/Controller/CovoiturageController.php
@@ -3,15 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\Rechercher;
+use App\Entity\Covoiturage;
 
 class CovoiturageController extends AbstractController
 {
-    #[Route('/covoiturage', name: 'app_covoiturage')]
-    public function index(): Response
+    #[Route('/covoiturage', name: 'app_covoiturage', methods:['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+
+        $covoiturage = new Covoiturage();
+        $form = $this->createForm(Rechercher::class, $covoiturage); 
+        $form->handleRequest($request); 
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $data = $form->getData(); 
+            return $this->redirectToRoute('app_covoiturage', [], response::HTTP_SEE_OTHER); 
+        } 
+        
         return $this->render('covoiturage/covoiturage.html.twig', [
+            'form' => $form->createView(),
             'controller_name' => 'CovoiturageController',
         ]);
     }
